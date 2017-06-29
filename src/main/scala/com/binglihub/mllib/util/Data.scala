1e507df928cfdd2c87f7326608abc6c563ae3d0d
Bing Li <bingli8882@gmail.com>
2017-06-28 21:08:06 -0400

    create Data class  Changes to be committed: 	new file:   src/main/scala/com/binglihub/mllib/util/Data.scala

---
+++ src/main/scala/com/binglihub/mllib/util/Data.scala
@@ -0,0 +1,10 @@
+package com.binglihub.mllib.util
+
+/**
+  * This class contains a data set.
+  * @param dataSet a 2D array contains data
+  * @tparam T the type of data
+  */
+case class Data[T](val dataSet: Array[Array[T]]) {
+
+}
