9920731be88d6671e95cae390b9b0c7b2c031836
Bing Li <bingli8882@gmail.com>
2017-06-28 21:07:14 -0400

    add euclidean function and test  Changes to be committed: 	new file:   src/main/scala/com/binglihub/mllib/kmeans/KMeans.scala 	new file:   src/test/scala/com/binglihub/mllib/test/KMeanTest.scala

---
+++ src/main/scala/com/binglihub/mllib/kmeans/KMeans.scala
@@ -0,0 +1,25 @@
+package com.binglihub.mllib.kmeans
+
+/**
+  * This object contains KMeans functions
+  */
+object KMeans {
+  /**
+    * Euclidean distance function
+    * @param a vector a
+    * @param b vector b
+    * @return a Some[Double] value represents the distance between a and b, or None for wrong input vectors
+    */
+  def euclidean(a:Array[Double],b:Array[Double]):Option[Double]=
+    if (a==null || b ==null||a.length!=b.length) None
+    else Some(
+      Math.sqrt(
+        (0 until a.length).foldLeft(0.0){
+          case(value:Double,index:Int) => value+(a(index)-b(index))*(a(index)-b(index))
+        }
+      )
+    )
+
+
+
+}
---
+++ src/test/scala/com/binglihub/mllib/test/KMeanTest.scala
@@ -0,0 +1,21 @@
+package com.binglihub.mllib.test
+
+import org.scalatest.{FlatSpec, Matchers}
+import com.binglihub.mllib.kmeans.KMeans._
+
+/**
+  * KMeans test functions
+  */
+class KMeanTest extends FlatSpec with Matchers{
+
+  "The euclidean function" should "return None" in {
+    euclidean(null,Array(0.0)) should be (None)
+    euclidean(Array(0.0),null) should be (None)
+    euclidean(Array(0.1),Array(0.1,0.2)) should be (None)
+  }
+
+  it should "return Some(5)" in {
+    Math.abs(euclidean(Array(0.0,3.0),Array(4.0,0.0)).get - 5.0) should be < 0.00001
+  }
+
+}
